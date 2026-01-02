1496773c3496d0e5755d3553f11153a93c09b2b5
muhammadkh97 <muhammed.kh97@gmail.com>
2026-01-02 06:14:48 +0200

    fix: resolve all merge conflicts in type definitions and driver settings

--- types/leaflet.d.ts
+++ types/leaflet.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Leaflet Type Definitions Extension
 declare module 'leaflet' {
   export * from '@types/leaflet';
@@ -9,15 +8,3 @@ declare module 'leaflet' {
     }
   }
 }
-=======
-// Leaflet Type Definitions Extension
-declare module 'leaflet' {
-  export * from '@types/leaflet';
-  
-  namespace Icon {
-    interface Default {
-      _getIconUrl?: string;
-    }
-  }
-}
->>>>>>> 59cb1431e448110cfe49ca35fb79aa53e9d8b18b
--- types/qrcode.react.d.ts
+++ types/qrcode.react.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 declare module 'qrcode.react' {
   import { CSSProperties, Component } from 'react'
 
@@ -27,33 +26,3 @@ declare module 'qrcode.react' {
 
   export default class QRCode extends Component<QRCodeProps> {}
 }
-=======
-declare module 'qrcode.react' {
-  import { CSSProperties, Component } from 'react'
-
-  export interface QRCodeProps {
-    value: string
-    size?: number
-    level?: 'L' | 'M' | 'Q' | 'H'
-    includeMargin?: boolean
-    renderAs?: 'canvas' | 'svg'
-    bgColor?: string
-    fgColor?: string
-    imageSettings?: {
-      src: string
-      x?: number
-      y?: number
-      height?: number
-      width?: number
-      excavate?: boolean
-    }
-    style?: CSSProperties
-    className?: string
-  }
-
-  export class QRCodeCanvas extends Component<QRCodeProps> {}
-  export class QRCodeSVG extends Component<QRCodeProps> {}
-
-  export default class QRCode extends Component<QRCodeProps> {}
-}
->>>>>>> 59cb1431e448110cfe49ca35fb79aa53e9d8b18b
